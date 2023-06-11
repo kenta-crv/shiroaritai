1461a0d63c4ce5772a4514ea443317a3ee36a14b
kenta-crv <okuyamakenta@gmail.com>
2023-06-11 14:37:00 +0900

    fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,16 +1,10 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://xn--new351c2sh.net/"
+SitemapGenerator::Sitemap.default_host = "https://factolink.jp/"
 
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
 add "/" , changefreq: 'daily', priority: 1.0
-add "/asahi" , changefreq: 'daily', priority: 0.8
-add "/cocacola" , changefreq: 'daily', priority: 0.8
-add "/dydo" , changefreq: 'daily', priority: 0.8
-add "/itoen" , changefreq: 'daily', priority: 0.8
-add "/kirin" , changefreq: 'daily', priority: 0.8
-add "/otsuka" , changefreq: 'daily', priority: 0.8
 
   add columns_path, :priority => 1.0, :changefreq => 'daily'
   Column.find_each do |column|
