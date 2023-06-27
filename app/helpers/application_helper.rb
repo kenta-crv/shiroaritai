288564e1389e09f6a4cb5a6c61b35d12197702af
kenta-crv <okuyamakenta@gmail.com>
2023-06-27 13:10:20 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "ファクタリング一括比較見積なら『ファクタリンク』",
-      title:"<%= yield(:title) | ファクタリング一括見積' %>",
+      site: "",
+      title:"",
       description: "ファクタリング一括見積『ファクタリンク』は、完全無料で最安値のファクタリング会社を見つける事ができます。",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
